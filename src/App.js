e78145b759ce148e8cbf2c1b25c363ec19fe5e88
Rajat <rajat.2023@vitstudent.ac.in>
2025-09-07 23:28:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,6 +21,7 @@ function App() {
       showAlert("Dark mode has been enabled");
     } else {
     document.getElementsByTagName("body")[0].style.backgroundColor ="white";
+       document.getElementsByTagName("body")[0].style.color ="black"; 
       newtext1("Enable dark");
       showAlert("Light mode has been enabled");
     }
